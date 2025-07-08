245f78d4b8591eeb1ffd5652973ea0060688240a
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-07-08 15:47:41 +0800

    scripts/service: disable sucompat at boot

--- module/service.sh
+++ module/service.sh
@@ -3,14 +3,20 @@ PATH=/data/adb/ksu/bin:$PATH
 MODDIR=${0%/*}
 
 USE_MOUNTIFY=false
-. "$MODDIR/config.sh" 
+SU_BINARY="$(busybox find $MODDIR/system -name "su")"
+
+[ -f "$MODDIR/config.sh" ] && . "$MODDIR/config.sh" 
+
+# wait for boot-complete
+until [ "$(getprop sys.boot_completed)" = "1" ]; do
+	sleep 1
+done
 
 if [ "$USE_MOUNTIFY" = "true" ]; then
 	sh "$MODDIR/mountify_standalone.sh" 
 fi
 
-if [ -f "$MODDIR/system/bin/su" ]; then
-	"$MODDIR/system/bin/su" --disable-sucompat
-else
-	"$MODDIR/system/product/bin/su" --disable-sucompat
-fi
+[ -f "$SU_BINARY" ] && "$SU_BINARY" --disable-sucompat
+
+
+# EOF
