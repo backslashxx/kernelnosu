09a8c9b13d55cc93d6cb29f4b2fc137ec850b71b
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-07-10 13:16:16 +0800

    su: migrate to zigcc compiled binaries

--- src/su.c
+++ src/su.c
@@ -1,18 +1,10 @@
-int _errno;
-#define SYS_ERRNO _errno
-#include "syscall.hpp"
+#include <string.h>
+#include <unistd.h>
+#include <sys/syscall.h>
+#include <fcntl.h>
 
-// armv7a-linux-androideabi21-clang su.c -m32 -static -nostdlib -o su -Os
-
-/*
- * $ ./android-ndk-r23b/toolchains/llvm/prebuilt/linux-x86_64/bin/armv7a-linux-androideabi21-clang su.c -static -nostdlib -o su -Oz -s
- * $ ls -la su
- * -rwxrwxr-x 1 user user 1748 Jul  9 16:08 su
- * $ /tmp/optane/openwrt-mr7350/staging_dir/host/bin/sstrip su
- * $ ls -la su
- * -rwxrwxr-x 1 user user 1024 Jul  9 16:08 su
-
-*/
+// zig cc -target arm-linux -s -Os su.c -static -Wl,--gc-sections
+// /tmp/optane/openwrt-mr7350/staging_dir/host/bin/sstrip a.out
 
 /*
  * strnmatch, test two strings if they match up to n len
@@ -21,6 +13,7 @@ int _errno;
  * 1 = not match
  * 
  */
+
 static int strnmatch(const char *a, const char *b, size_t count)
 {
 	// og condition was like *a && (*a == *b) && count > 0
@@ -40,25 +33,10 @@ static int strnmatch(const char *a, const char *b, size_t count)
 	return 0;
 }
 
-__attribute__((naked))
-void _start() {
-	__asm__ volatile (
-		"ldr r0, [sp]\n\t"
-		"add r1, sp, #4\n\t"
-		"add r2, r1, r0, lsl #2\n\t"
-		"add r2, r2, #4\n\t"
-
-		"bl main\n\t"
-		"mov r7, #1\n\t"
-		"svc #0\n\t"
-		"b .\n\t"
-	);
-}
-
 static int denied(void)
 {
 	const char *error = "Denied\n";
-	sys_write(2, error, strlen(error));
+	syscall(SYS_write, 2, error, strlen(error));
 	return 1;
 }
 
@@ -68,45 +46,38 @@ int main(int argc, const char **argv, const char **envp)
 	
 	if (argc >= 2 && !strnmatch(argv[0], "/data/adb", 9)
 		&& !strnmatch(argv[1], "--disable-sucompat", 18)) {
-		sys_prctl(0xdeadbeef, 15L, 0L, 0L, (unsigned long) &result);
+		syscall(SYS_prctl, 0xdeadbeef, 15L, 0L, 0L, (unsigned long) &result);
 		return 0;
 	}
 
-	// this costs 36 bytes
 	// if its called from /data/adb, dont continue!
-	// if (!strnmatch(argv[0], "/data/adb", 9))
-	// 	return denied();
+	if (!strnmatch(argv[0], "/data/adb", 9))
+	 	return denied();
 
-	sys_prctl(0xdeadbeef, 0L, 0L, 0L, (unsigned long) &result);
+	syscall(SYS_prctl, 0xdeadbeef, 0L, 0L, 0L, (unsigned long) &result);
 	if (result != 0xdeadbeef)
 		return denied();
 
 	
-	// const char *args[] = { "/system/bin/su" };
-	// if (argc < 1) argv = args;
-	// else argv[0] = "/system/bin/su";
+	const char *args[] = { "/system/bin/su" };
+	if (argc < 1) argv = args;
+	else argv[0] = "/system/bin/su";
 	
 	// lets just segfault if user passes argc == 0
 	// that prolly doesnt happen
-	argv[0] = "/system/bin/su";
+	// argv[0] = "/system/bin/su";
 	
+	// https://syscalls.mebeim.net/?table=arm64/64/aarch64/v5.0
+	// openat is available on both arm and aarch64
 	char *debug_msg = "KernelSU: kernelnosu su->ksud\n";
-	int fd = sys_open("/dev/kmsg", 1, 0);
+	int fd = syscall(SYS_openat, -100, "/dev/kmsg", O_WRONLY, 0);
 	if (fd >= 0) {
-		sys_write(fd, debug_msg, strlen(debug_msg));
-		sys_close(fd);
+		syscall(SYS_write, fd, debug_msg, strlen(debug_msg));
+		syscall(SYS_close, fd);
 	}
-	
-	// sys_execve("/data/adb/ksud", argv, envp);	
-	// const char *error = "Failed to execve /data/adb/ksud\n";
-	// sys_write(2, error, strlen(error));
-	// return 1;
-	
-	sys_execve("/data/adb/ksud", argv, envp);
+
+	syscall(SYS_execve, "/data/adb/ksud", argv, envp);
 	
 	return denied();
 }
 
-// dummy for that 1024 meme
-__attribute__((section(".text"))) 
-const char dummy_padding[3];
