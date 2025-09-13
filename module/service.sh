008e2a2add8a20aacc2c4c00e263d83a3348c29e
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-09-13 22:47:06 +0800

    scripts/service: remove mountify logic at service

--- module/service.sh
+++ module/service.sh
@@ -2,16 +2,13 @@
 PATH=/data/adb/ksu/bin:$PATH
 MODDIR=${0%/*}
 
-USE_MOUNTIFY=false
 SU_BINARY="$(busybox find $MODDIR/system -name "su")"
 
-[ -f "$MODDIR/config.sh" ] && . "$MODDIR/config.sh" 
-
-if [ "$USE_MOUNTIFY" = "true" ]; then
-	sh "$MODDIR/mountify_standalone.sh" 
-fi
+# wait for boot-complete
+until [ "$(getprop sys.boot_completed)" = "1" ]; do
+	sleep 1
+done
 
 [ -f "$SU_BINARY" ] && "$SU_BINARY" --disable-sucompat
-
 
 # EOF
