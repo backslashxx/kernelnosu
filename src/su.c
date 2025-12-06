1bd604d7de05c208b683ccc3114183cbef23552e
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-12-06 20:12:46 +0800

    su: headerize

--- src/su.c
+++ src/su.c
@@ -1,73 +1,10 @@
-#include <sys/syscall.h>
 #include <sys/ioctl.h>
 #include <fcntl.h>
+#include "small_rt.h"
 
 // zig cc -target aarch64-linux su.c -Oz -s -Wl,--gc-sections,--strip-all,-z,norelro -fno-unwind-tables -Wl,--entry=__start -o su
 // /tmp/optane/openwrt-mr7350/staging_dir/host/bin/sstrip a.out
 
-#define NONE 0
-
-#if defined(__aarch64__)
-__attribute__((noinline))
-static long __syscall(long n,long a,long b,long c,long d,long e,long f)
-{
-	register long 
-		x8 asm("x8") = n,
-		x0 asm("x0") = a,
-		x1 asm("x1") = b,
-		x2 asm("x2") = c,
-		x3 asm("x3") = d,
-		x4 asm("x4") = e,
-		x5 asm("x5") = f;
-
-	asm volatile("svc #0"
-		:"=r"(x0)
-		:"r"(x8), "r"(x0), "r"(x1), "r"(x2), "r"(x3), "r"(x4), "r"(x5)
-		:"memory");
-
-	return x0;
-}
-
-#elif defined(__arm__)
-__attribute__((noinline))
-static long __syscall(long n, long a, long b, long c, long d, long e, long f) {
-	register long
-		r7 asm("r7") = n,
-		r0 asm("r0") = a,
-		r1 asm("r1") = b,
-		r2 asm("r2") = c,
-		r3 asm("r3") = d,
-		r4 asm("r4") = e,
-		r5 asm("r5") = f;
-
-	asm volatile("svc #0"
-		: "=r"(r0)
-		: "r"(r7), "r"(r0), "r"(r1), "r"(r2), "r"(r3), "r"(r4), "r"(r5)
-		: "memory");
-
-	return r0;
-}
-
-#elif defined(__x86_64__)
-__attribute__((noinline))
-static long __syscall(long n, long a, long b, long c, long d, long e, long f) {
-	long ret;
-	asm volatile(
-		"mov %5, %%r10\n"
-		"mov %6, %%r8\n"
-		"mov %7, %%r9\n"
-		"syscall"
-		: "=a"(ret)
-		: "a"(n), "D"(a), "S"(b), "d"(c), "r"(d), "r"(e), "r"(f)
-		: "rcx", "r11", 
-		"memory");
-
-	return ret;
-}
-#endif
-
-// https://gcc.gnu.org/onlinedocs/gcc/Library-Builtins.html
-// https://clang.llvm.org/docs/LanguageExtensions.html#builtin-functions
 #define memcmp __builtin_memcmp
 #define strlen __builtin_strlen
 
@@ -142,38 +79,9 @@ void prep_main(long *sp)
 {
 	long argc = *sp;
 	char **argv = (char **)(sp + 1);
-	char **envp = (char **)(sp + 2);
+	char **envp = argv + argc + 1; // we need to offset it by the number of argc's!
 
 	long exit_code = c_main(argc, argv, envp);
-	__syscall(SYS_exit, exit_code, 0, NONE, NONE, NONE, NONE);
+	__syscall(SYS_exit, exit_code, NONE, NONE, NONE, NONE, NONE);
 }
 
-
-// arch specific small entry points
-#if defined(__aarch64__)
-__attribute__((naked))
-void __start(void) {
-	asm volatile(
-		"mov x0, sp\n"
-		"b prep_main\n"
-	);
-}
-
-#elif defined(__arm__)
-__attribute__((naked))
-void __start(void) {
-	asm volatile(
-		"mov r0, sp\n"
-		"b prep_main\n"
-    );
-}
-
-#elif defined(__x86_64__)
-__attribute__((naked, section(".text.start"))) 
-void __start(void) {
-	asm volatile(
-		"mov %rsp, %rdi\n"
-		"jmp prep_main\n"
-	);
-}
-#endif
